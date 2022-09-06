2bbfc2d2c0a1f4891c67391a597042adee6cf535
isadfrn <isadfrn@gmail.com>
2022-09-06 14:42:56 -0300

    feat: add custom internal server error page

---
+++ client/pages/500.js
@@ -0,0 +1,15 @@
+import { Container } from '../styles/400';
+import { Main, TitleContainer, Title, Subtitle } from '../styles/index';
+
+export default function Custom500() {
+  return (
+    <Container>
+      <Main>
+        <TitleContainer>
+          <Title>500</Title>
+          <Subtitle>Internal server error</Subtitle>
+        </TitleContainer>
+      </Main>
+    </Container>
+  );
+}
