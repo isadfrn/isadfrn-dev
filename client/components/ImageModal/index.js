ddd61a4873a57a43d666f92abcfd3e125ea4014d
isadfrn <isadfrn@gmail.com>
2022-09-23 20:05:08 -0300

    feat: add images

--- client/components/ImageModal/index.js
+++ client/components/ImageModal/index.js
@@ -1,9 +1,12 @@
+import Image from 'next/image';
+
 import { Container } from './styles';
 
-export function ImageModal({ closeFunction }) {
+export function ImageModal({ openCloseFunction, imageToShow }) {
   return (
     <Container>
-      <button onClick={closeFunction}>X</button>
+      <button onClick={openCloseFunction}>X</button>
+      <Image src={imageToShow} alt="An image on timeline" />
     </Container>
   );
 }
